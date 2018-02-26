cd3b76ec9dc430d3f8938e2151eb107151cf688e
Lucas Novaes <lanovaes1@gmail.com>
2018-02-26 15:48:12 -0300

    Responsable for getting adresses and returning into simple models

--- Easy/Easy/AddressSearchController/AddressSearchController.swift
+++ Easy/Easy/AddressSearchController/AddressSearchController.swift
@@ -7,27 +7,41 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
+
+protocol AddressSearchControllerDelegate: class{
+    func addressSearchController(_ controller: AddressSearchController, didSelect prediction: GMSAutocompletePrediction)
+}
 
 final class AddressSearchController: UIViewController {
 
-    let tableView: UITableView
-    let viewModel: AddressSearchViewModel
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        tableView.backgroundColor = UIColor.gray
-        tableView.showsVerticalScrollIndicator = false
-        
-        tableView.delegate = self
-        tableView.dataSource = self
+    struct TableViewIdentifier{
+        static var predictionsCellIdentifier = "AdressesPredictionCell"
+        static var predictionsCell = "AdressesPredictionTableViewCell"
     }
     
-    init(){
+    let tableView: UITableView
+    let viewModel: AddressSearchViewModel
+    let type: SearchType
+    
+    var searchController: UISearchController
+    
+    weak var delegate: AddressSearchControllerDelegate?
+
+    enum SearchType: String{
+        case source = "Source"
+        case destination = "Destination"
+    }
+    
+    init(type: SearchType){
         tableView = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
-        viewModel = DestinationSearchViewModel()
-        
-        super.init(nibName: nil, bundle: nil)
+        viewModel = AddressSearchViewModel()
+        searchController = UISearchController(searchResultsController: nil)
+
+        self.type = type
+
+        super.init(nibName: nil, bundle: nil)        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -39,21 +53,68 @@ final class AddressSearchController: UIViewController {
         self.view.addSubview(tableView)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchController.searchBar.placeholder = type.rawValue
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+        if #available(iOS 11.0, *) {
+            navigationItem.searchController = searchController
+        }
+        definesPresentationContext = true
+        
+        tableView.showsVerticalScrollIndicator = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: TableViewIdentifier.predictionsCell, bundle: nil), forCellReuseIdentifier: TableViewIdentifier.predictionsCellIdentifier)
+        tableView.dynamicSize()
+    }
+  
+    // Make UISearchController active without have to scroll down
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if #available(iOS 11.0, *) {
+            navigationItem.hidesSearchBarWhenScrolling = false
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if #available(iOS 11.0, *) {
+            navigationItem.hidesSearchBarWhenScrolling = true
+        }
+    }
+    
 }
 
 extension AddressSearchController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return viewModel.numberOfPredictions(at: section)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: TableViewIdentifier.predictionsCellIdentifier) as! AdressesPredictionTableViewCell
+        cell.fill(with: viewModel.predictionText(at: indexPath))
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        delegate?.addressSearchController(self, didSelect: viewModel.prediction(at: indexPath))
     }
     
 }
 
+extension AddressSearchController: UISearchResultsUpdating{
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        viewModel.findAddres(with: searchController.searchBar.text) { (placesPrediction, error) in
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+}
--- Easy/Easy/AddressSearchController/AddressSearchViewModel.swift
+++ Easy/Easy/AddressSearchController/AddressSearchViewModel.swift
@@ -8,8 +8,12 @@
 
 import Foundation
 import UIKit
+import GooglePlaces
 
 final class AddressSearchViewModel{
+    
+    private let gmsPlacesClient = GMSPlacesClient()
+    var predictions: [GMSAutocompletePrediction] = []
     
     func viewConfiguration() -> UIView{
         let view = UIView()
@@ -17,6 +21,43 @@ final class AddressSearchViewModel{
         view.isOpaque = true
         
         return view
+    }
+    
+    func numberOfPredictions(at section: Int) -> Int{
+        return predictions.count
+    }
+    
+    func predictionText(at indexPath: IndexPath) -> NSAttributedString{
+        return predictions[indexPath.row].attributedFullText
+    }
+    
+    func prediction(at indexPath: IndexPath) -> GMSAutocompletePrediction{
+        return predictions[indexPath.row]
+    }
+    
+    func findAddres(with text: String?, onComplete: @escaping ([GMSAutocompletePrediction], Error?) -> Void){
+        guard let text = text else {
+            
+            // set status null of search! User did no typed anything
+            
+            return
+        }
+        
+        let filter = GMSAutocompleteFilter()
+        filter.type = .address
+        
+        gmsPlacesClient.autocompleteQuery(text, bounds: nil, filter: filter) { [weak self] (predictions, error) in
+            if let error = error{
+                print("Error in autocomplete: \(error)")
+                onComplete([], error)
+                return
+            }
+        
+            if let predictions = predictions {
+                self?.predictions = predictions
+                onComplete(predictions, nil)
+            }
+        }
     }
     
 }
